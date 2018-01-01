0d8ccbc0fc7d01eff88f2d9c298a73cb4bea92e6
Vapauden Kuolemasta <kuolemasta@mail.ru>
2018-01-01 14:07:13 +0300

    Now work correctly.

--- picasa.py
+++ picasa.py
@@ -32,44 +32,44 @@ class Picasa:
 
             for album in albumList:
                 if album == 'albumid': continue
+                self.filePath = os.path.join(self.path if self.path else '', id, album)
+                os.makedirs(self.filePath, exist_ok=True)
                 self.download_albums(url + '/album/' + album)
 
     def download_albums(self, albumUrl):
         html = urllib.request.urlopen(albumUrl).read().decode('utf-8')
 
-        storage = re.search('var storage = "(.*)";', html)
-        if not storage:
-            print(albumUrl + ' - No photo found or locked album!')
+        entryList = re.findall('<entry>(.*?)<\/entry>', html)
+        if not entryList:
+            print(albumUrl + ' - No media was found!')
             return
-
-        self.videoStorage = None
-        self.storage = storage.group(1)
-        self.securityCode = re.search('var albumSecurityCode = \"(.*)\";', html).group(1)
-
-        links = re.findall('{(\"photoID\".*?)}', html)
-        if not links:
-            print(albumUrl + ' - No photo found!')
-            return
-
-        m = re.search('([\w-]+?)\.rajce\.idnes\.cz/(.*?)/', albumUrl)
-        self.filePath = os.path.join(self.path if self.path else '', m.group(1), m.group(2).replace('.', '_'))
-        os.makedirs(self.filePath, exist_ok=True)
 
         pool = Pool(self.THREADS_COUNT)
-        pool.map(self.download_file, links)
+        pool.map(self.download_entry, entryList)
         pool.close()
         pool.join()
 
         print(albumUrl + ' - OK!')
 
-    def download_file(self, fileUrl):
-        if re.search('\"isVideo\":(.*),\"desc', fileUrl).group(1) == 'false':
-            fileName = re.search('\"fileName\":\"(.+?)\"', fileUrl).group(1)
-            fileUrl = self.storage + 'images/' + fileName
-        else:
-            fileName = re.search('\"info\":\"(.+?\..+?)[\s\"]', fileUrl).group(1)
-            photoID = re.search('\"photoID\":\"(\d+)', fileUrl).group(1)
-            fileUrl = self.videoStorage + photoID
+    def download_entry(self, entry):
+        urlList = re.findall('<media:content url=\'(.*?)\'', entry)
+        if not urlList:
+            print('No media in current entry!')
+            return
+
+        fileName = ''
+        videoSize = 0
+        fileUrl = ''
+        for url in urlList:
+            name = re.search('.*/(.*?)$', url).group(1)
+
+            if name.find("=") == -1:
+                fileName = name
+                fileUrl = url if fileUrl == '' else fileUrl
+                continue
+
+            if re.search('=m(\d+)', url) and int(re.search('=m(\d+)', url).group(1)) > videoSize:
+                fileUrl = url
 
         try:
             urllib.request.urlretrieve(fileUrl, os.path.join(self.filePath, fileName))
@@ -79,10 +79,8 @@ class Picasa:
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-i', '--ids', help="List of IDs", nargs='+', required = True)
+    parser.add_argument('-i', '--ids', help="List of IDs", nargs='+', required=True)
     parser.add_argument('-p', '--path', help="Destination folder")
-    # args = parser.parse_args()
-    args = parser.parse_args('-i 110046880579284558208'.split())
-    # args = parser.parse_args('-i http://dolfik88.rajce.idnes.cz/ -p E:/Downloads/Rajce'.split())
+    args = parser.parse_args()
     downloader = Picasa(args.ids, args.path)
     downloader.download()
